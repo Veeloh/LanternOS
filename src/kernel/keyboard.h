dfc6a223a8655c0df1a291a128b298c7c4d255a6
Veeloh <102321623+Veeloh@users.noreply.github.com>
2026-07-07 21:58:41 -0600

    Update keyboard.h

--- src/kernel/keyboard.h
+++ src/kernel/keyboard.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <stdint.h>
 
+#define KEY_UP      0x11
+#define KEY_DOWN    0x12
+#define KEY_LEFT    0x13
+#define KEY_RIGHT   0x14
+#define KEY_CTRL_C  0x03   // conventional ETX code for Ctrl-C
+
 extern char character_map[];
 
 void keyboard_init();
