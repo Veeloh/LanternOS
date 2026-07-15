14be67e03c4c71b1da3f70742a2aab3872eba850
Veeloh <102321623+Veeloh@users.noreply.github.com>
2026-07-14 19:15:50 -0600

    Update clock.h

--- src/kernel/clock.h
+++ src/kernel/clock.h
@@ -5,3 +5,4 @@ void clock_init(uint8_t h, uint8_t m, uint8_t s);
 void clock_tick();
 void clock_draw();
 void clock_set(uint8_t h, uint8_t m, uint8_t s);
+void clock_get(uint8_t* h, uint8_t* m, uint8_t* s);
